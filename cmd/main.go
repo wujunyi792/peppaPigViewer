7bec4c1bd1c7bd9f8bbbe55470bf7f412583459a
Esonhugh <esonhughoutside@gmail.com>
2022-12-22 14:47:32 +0800

    update(commandline): using go runtime for control Current Status

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/robfig/cron"
+	"log"
 	"newJwCourseHelper/internal/config"
 	"newJwCourseHelper/internal/core"
 	"os"
@@ -15,21 +16,12 @@ import (
 
 var configFile = flag.String("f", "config.json", "Specify the config file")
 
-func main() {
-	flag.Parse()
-	var c []config.Config
-	var users []*core.User
+var quit = make(chan os.Signal)
+var users []*core.User
+var c []config.Config
 
-	content, err := os.ReadFile(*configFile)
-	if err != nil {
-		panic(err)
-	}
-
-	if err = json.Unmarshal(content, &c); err != nil {
-		panic(err)
-	}
-
-	//c中的每一个元素都是一个config.Config结构体，保存了用户的配置、任务等
+// Auto ChooseCourse func will start with new go runtime
+func ChooseCourse() {
 	for _, user := range c {
 		res, e := core.LoadConfig(user).LoginPW(user.User.StaffId, user.User.Password) //用户登录（TODO:不需要多次登录，可以直接传入Token）
 		if e != nil {
@@ -52,11 +44,57 @@ func main() {
 		users[i].GetCorn().Start()
 	}
 
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// if quit Print Information
 	<-quit
-
 	for i := 0; i < len(users); i++ {
 		users[i].GetCorn().Stop()
 	}
+}
+
+// Command Line Execute for relogin and list Current Status
+func Execute() {
+	for {
+		var cmd string
+		fmt.Printf("Console> ")
+		_, _ = fmt.Scanln(&cmd)
+
+		switch cmd {
+		case "list":
+			for i := 0; i < len(users); i++ {
+				log.Printf("This is the User #%v Chosen CourseList \n")
+				users[i].PrintCourseChosenList()
+			}
+		case "relogin":
+			for k, user := range users {
+				var err error
+				users[k], err = user.ForceRetryAuth()
+				if err != nil {
+					log.Printf("Re Login user[%v] got error: %v \n", k, err)
+				}
+			}
+		case "quit":
+			quit <- syscall.SIGTERM
+			os.Exit(0)
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	content, err := os.ReadFile(*configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = json.Unmarshal(content, &c); err != nil {
+		panic(err)
+	}
+
+	//c中的每一个元素都是一个config.Config结构体，保存了用户的配置、任务等
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go ChooseCourse()
+	Execute()
 }
